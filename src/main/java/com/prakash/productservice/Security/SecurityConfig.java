aba80d8d7e2ea490b61a0dabdf75b88500883b15
prakash <dizprakash@gmail.com>
2026-04-30 11:39:16 +0545

    '

--- src/main/java/com/prakash/productservice/Security/SecurityConfig.java
+++ src/main/java/com/prakash/productservice/Security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( auth -> auth
                         .requestMatchers("/api/products/**").permitAll()
+                        .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
